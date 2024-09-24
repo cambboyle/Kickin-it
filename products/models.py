c7b4729896b0effc988e7e8a460cdc45bdc77ec1
Cameron Boyle <cambboyle@gmail.com>
2024-09-24 16:16:14 +0000

    Add category filtering for products

--- products/models.py
+++ products/models.py
@@ -28,6 +28,8 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     is_featured = models.BooleanField(default=False, null=True, blank=True)
+    is_sale = models.BooleanField(default=False, null=True, blank=True)
+    is_new = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
@@ -10,8 +10,42 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
+# Product filtering
     if request.GET:
+
+        # Filter by category
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        # Filter by brand
+        if 'brand' in request.GET:
+            brand = request.GET['brand']
+            products = products.filter(brand=brand)
+
+        # Filter by other brands
+        if 'other_brands' in request.GET:
+            known_brands = ['Nike', 'Adidas', 'New Balance', 'Vans', 'Puma', 'Asics', 'Converse', 'Reebok', 'Hoka One One', 'On Running']
+            products = products.exclude(brand__in=known_brands)
+            
+        # Filter by sale items
+        if 'is_sale' in request.GET:
+            is_sale = request.GET['is_sale'].lower() == 'true'
+            products = products.filter(is_sale=is_sale)
+
+        # Filter by new items
+        if 'is_new' in request.GET:
+            is_new = request.GET['is_new'].lower() == 'true'
+            products = products.filter(is_new=is_new)
+
+        # Filter by gender
+        if 'gender' in request.GET:
+            gender = request.GET['gender']
+            products = products.filter(Q(gender__iexact=gender) | Q(gender__iexact='unisex'))
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +58,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
