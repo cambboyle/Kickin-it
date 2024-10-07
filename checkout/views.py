ff924e0dfa2c34f5deb87324122d8831bf07f50e
Cameron Boyle <cambboyle@gmail.com>
2024-10-07 14:43:32 +0000

    Add stripe element js file

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q7HgTRrrhrzfh6oMNuhJXZzeotxceKP4nSXHunKCkwL1Mc3hGC5sK6jEJJNnhkorbe6uRvDmHdwSeJpbXpSgF6T00LTqI1Nfk',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
