d0fe894ebe61fe937c7b5a976823a1aed2e28578
Cameron Boyle <cambboyle@gmail.com>
2024-10-15 12:43:41 +0000

    Deploy app to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kickin_it.wsgi:application
--- kickin_it/settings.py
+++ kickin_it/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-*r-pd#&zsrjx-#r*=rc82610%_vs-klk&_=z7a%2k7)_^=**1)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-cambboyle-kickinitv1-rjuiu83853b.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['kickin-it.herokuapp.com', 'localhost', 'https://8000-cambboyle-kickinitv1-rjuiu83853b.ws.codeinstitute-ide.net']
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-cambboyle-kickinitv1-rjuiu83853b.ws.codeinstitute-ide.net'
@@ -114,7 +115,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -126,12 +127,17 @@ WSGI_APPLICATION = 'kickin_it.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+    }
 
 
 # Password validation
