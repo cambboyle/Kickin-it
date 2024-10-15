b06da9c41d53bda79eac40c0c48b9124c897dd98
Cameron Boyle <cambboyle@gmail.com>
2024-10-15 13:04:55 +0000

    Remove secret key and set debug

--- kickin_it/settings.py
+++ kickin_it/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*r-pd#&zsrjx-#r*=rc82610%_vs-klk&_=z7a%2k7)_^=**1)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'kickin-it.herokuapp.com',
