acf90e415bed750ed3e284671f9f57c9897ccdd8
Cameron Boyle <cambboyle@gmail.com>
2024-10-22 08:18:04 +0000

    Update debug to false

--- kickin_it/settings.py
+++ kickin_it/settings.py
@@ -22,15 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# FOR DEVELOPMENT ONLY
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-*r-pd#&zsrjx-#r*=rc82610%_vs-klk&_=z7a%2k7)_^=**1)'
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-
-# Uncomment this for deployment
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 ALLOWED_HOSTS = [
